0c95d0ac42a6e1747be64ceb903da1ad8643e15f
Otavio Santana <otaviopolianasantana@gmail.com>
2022-09-14 06:32:27 +0100

    update header
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/main/java/jakarta/data/repository/package-info.java
+++ api/src/main/java/jakarta/data/repository/package-info.java
@@ -18,7 +18,7 @@
 /**
  * In Domain-driven design, DDD, a repository is an object that participates in the domain but abstracts away storage
  * and infrastructure details. Most systems have persistent storage,
- * like a database, for its full functioning. Applying repositories happens
+ * like a database, for their full functioning. Applying repositories happens
  * by integrating and synchronizing with existing aggregate objects in the system.
  */
 package jakarta.data.repository;
