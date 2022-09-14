7e6706319cd6e6ed4797f553c4da85e18836d564
Otavio Santana <otaviopolianasantana@gmail.com>
2022-09-14 06:33:18 +0100

    Add package-info on the jakarta.data
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/main/java/jakarta/data/package-info.java
+++ api/src/main/java/jakarta/data/package-info.java
@@ -17,6 +17,8 @@
  */
 
 /**
- *
+ *Jakarta Data provides an API that makes data access easy. A Java developer can split the persistence
+ * from the model with several features, such as the ability to compose custom query methods
+ * on a Repository interface where the framework will implement it.
  */
 package jakarta.data;
