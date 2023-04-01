c1b07253dab0562fc1a929420e78f11ded0d28fd
Elias Nogueira <elias.nogueira@gmail.com>
2023-04-01 17:51:35 +0200

    added new test for the Pagination class
    Signed-off-by: Elias Nogueira <elias.nogueira@gmail.com>

---
+++ api/src/test/java/jakarta/data/repository/PaginationTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+package jakarta.data.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PaginationTest {
+
+    @Test
+    @DisplayName("Should throw IllegalArgumentException when no keyset values were provided")
+    void shouldThrowExceptionWhenNoKeysetValuesWereProvided() {
+        assertThatIllegalArgumentException()
+                .as("Mode must be different from OFFSET when cursor is null")
+                .isThrownBy(() -> new Pagination(1, 10, Collections.emptyList(), Pageable.Mode.CURSOR_NEXT, null));
+    }
+
+    @Test
+    @DisplayName("Should throw UnsupportedOperationException when keyset is not supported")
+    void shouldThrowExceptionWhenKeysetIsNotSupported() {
+        assertThatThrownBy(() -> {
+            Pagination pagination = new Pagination(1, 10, Collections.emptyList(), Pageable.Mode.CURSOR_NEXT, new KeysetCursor("me", 200));
+            pagination.next();
+        }).isInstanceOf(UnsupportedOperationException.class)
+                .hasMessage("Not supported for keyset pagination. Instead use afterKeyset or afterKeysetCursor to provide the next keyset values or obtain the nextPageable from a KeysetAwareSlice.");
+    }
+}
