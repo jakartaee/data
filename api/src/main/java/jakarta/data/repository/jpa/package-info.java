5988c79240aebb4cdc0581f77ab87812446a9de3
Otavio Santana <otaviopolianasantana@gmail.com>
2023-09-01 06:33:51 +0100

    feat: create a domain to JPA
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ api/src/main/java/jakarta/data/repository/jpa/package-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * This package contains interfaces and classes that facilitate working with repositories
+ * in the Jakarta Persistence API (JPA) context. JPA is a Java specification for * managing relational data in a database.
+ * Repositories in this package leverage JPA to provide easy and standardized access to persistent entities.
+ */
+package jakarta.data.repository.jpa;
