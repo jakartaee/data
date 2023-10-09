cf60d689ad68e150b7584dff6d244b39d1bc6795
Otavio Santana <otaviopolianasantana@gmail.com>
2023-10-09 13:43:11 +0100

    docs: update documentation about exception
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/main/java/jakarta/data/Insert.java
+++ api/src/main/java/jakarta/data/Insert.java
@@ -57,7 +57,7 @@ import java.lang.annotation.Target;
  * </p>
  *
  * <p>If this annotation is combined with other operation annotations (e.g., {@code @Update}, {@code @Delete},
- *  {@code @Save}), it will throw an {@link UnsupportedOperationException} as only one operation type can be specified.</p>
+ *  {@code @Save}), it will throw an {@link IllegalStateException} as only one operation type can be specified.</p>
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
--- api/src/main/java/jakarta/data/repository/Save.java
+++ api/src/main/java/jakarta/data/repository/Save.java
@@ -56,12 +56,13 @@ import java.lang.annotation.Target;
  *     Car park(Car car);
  * }
  * </pre>
- * <p>The {@code @Insert} annotation can be used to indicate that the {@code park(Car)} method is responsible
+ * <p>The {@code @Update} annotation can be used to indicate that the {@code park(Car)} method is responsible
  * for updating the entity in the database if it already exists there and otherwise inserting
  * a car entity into a database.
  * </p>
  *
- *
+ * <p>If this annotation is combined with other operation annotations (e.g., {@code @Update}, {@code @Delete},
+ *  {@code @Insert}), it will throw an {@link IllegalStateException} as only one operation type can be specified.</p>
  * @see jakarta.data.exceptions.OptimisticLockingFailureException
  */
 @Documented
