1d98283594450beaa7caedc9e4587a6c5899ae30
Otavio Santana <otaviopolianasantana@gmail.com>
2023-10-11 23:02:52 +0300

    docs: update the upate annotations
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/main/java/jakarta/data/repository/Update.java
+++ api/src/main/java/jakarta/data/repository/Update.java
@@ -35,9 +35,23 @@ import java.lang.annotation.Target;
  *     <li>An array of entities to be updated.</li>
  * </ul>
  * <p>The return type of the annotated method must be {@code void}, {@code boolean}, a numeric primitive type
- * (such as {@code int}), or a corresponding primitive wrapper type (such as {@link Integer}).
+ * (such as {@code int}), a corresponding primitive wrapper type (such as {@link Integer}), or the same type as the parameter.
+ * </p>
+ * <p>
+ * The return type of an annotated method that accepts an {@code Iterable} or array of entities as the parameter must
+ * have a return type that is {@code void}, {@code Void}, or an {@code Iterable} or array of the entity.
  * A boolean return type indicates whether a matching entity was found in the database to update.
  * A numeric return type indicates how many matching entities were found in the database to update.
+ * The return type of an annotated method that accepts an {@code Iterable} or array of entities as the parameter must
+ * have a return type that is {@code void}, {@code Void}, or an {@code Iterable} or array of the entity.
+ * For example, if the method is annotated with {@code @Update} and takes a parameter of type {@code Car car},
+ * the return type can be {@code Car}.
+ * Similarly, if the parameter is an {@code Iterable<Car>} or an array of {@code Car}, the return type can be
+ * {@code Iterable<Car>}.
+ * Entities that are returned by the annotated method must include all values that were
+ * written to the database, including all automatically generated values, update versions and incremented values
+ * that changed due to the update. The position of entities within an {@code Iterable} or array return value
+ * must correspond to the position of entities in the parameter based on the unique identifier of the entity.
  * </p>
  * <p>Updating an entity involves modifying its existing data in the database. The method will search for the entity
  * in the database using its ID (and version, if versioned) and then update the corresponding record with the new data. After invoking
