533d2ca668b36b2bd56a07d5841740fd227464d1
Fanon Jupkwo <jupsfan@gmail.com>
2023-10-25 04:30:44 +0100

    implicit testing

--- api/src/test/java/jakarta/data/displaynamegeneration/test/ClassAnnotatedWithReplaceCamelCaseAndUnderscoreAndNumberTest.java
+++ api/src/test/java/jakarta/data/displaynamegeneration/test/ClassAnnotatedWithReplaceCamelCaseAndUnderscoreAndNumberTest.java
@@ -18,55 +18,98 @@
 package jakarta.data.displaynamegeneration.test;
 
 import jakarta.data.displaynamegeneration.ReplaceCamelCaseAndUnderscoreAndNumber;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-//@DisplayNameGeneration(ReplaceCamelCaseAndUnderscoreAndNumber.class)
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+//TODO: Solve the module issue related to the usage of @DisplayNameGeneration(ReplaceCamelCaseAndUnderscoreAndNumber.class)
+// module jakarta.data does not "exports jakarta.data.displaynamegeneration" to unnamed module
+// and TestEngine with ID 'junit-jupiter' failed to discover tests
 
 /**
- * This class is used to simulate the usage of the custom generator {@linkplain ReplaceCamelCaseAndUnderscoreAndNumber }.
- */
+ * This class implicitly tests {@linkplain ReplaceCamelCaseAndUnderscoreAndNumber } by checking if the generated display name matches the expected one.
+ * */
+@Disabled("Disabled until module issue has been fixed!")
+//This next line is commented in order to avoid "TestEngine with ID 'junit-jupiter' failed to discover tests"
+//@DisplayNameGeneration(ReplaceCamelCaseAndUnderscoreAndNumber.class)
 class ClassAnnotatedWithReplaceCamelCaseAndUnderscoreAndNumberTest {
+
+    private TestInfo testInfo;
+
+    @BeforeEach
+    void init(TestInfo testInfo) {
+        this.testInfo = testInfo;
+    }
 
     @Test
     void shouldReturnErrorWhen_maxResults_IsNegative() {
+        assertSoftly(softly -> {
+            softly.assertThat(testInfo.getDisplayName()).isEqualTo("Should return error when maxResults is negative");
+        });
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"", "  "})
     void shouldCreateLimitWithRange(String input) {
         methodNotAnnotatedWithTestOrParameterizedTest();
+        assertSoftly(softly -> {
+            softly.assertThat(testInfo.getDisplayName()).isEqualTo("Should create limit with range (String)");
+        });
     }
 
     @ParameterizedTest
     @ValueSource(ints = {5, 23})
     void shouldReturn5Errors(int input) {
+        assertSoftly(softly -> {
+            softly.assertThat(testInfo.getDisplayName()).isEqualTo("Should return 5 errors (int)");
+        });
     }
 
     @Test
     void shouldReturn5errors() {
+
         methodNotAnnotatedWithTestOrParameterizedTest();
+        assertSoftly(softly -> {
+            softly.assertThat(testInfo.getDisplayName()).isEqualTo("Should return 5errors");
+        });
     }
 
     @Test
     void shouldReturn23Errors() {
+        methodNotAnnotatedWithTestOrParameterizedTest();
+        assertSoftly(softly -> {
+            softly.assertThat(testInfo.getDisplayName()).isEqualTo("Should return 23 errors");
+        });
     }
 
     @Test
     void shouldReturnTheValueOf_maxResults() {
         methodNotAnnotatedWithTestOrParameterizedTest();
+        assertSoftly(softly -> {
+            softly.assertThat(testInfo.getDisplayName()).isEqualTo("Should return the value of maxResults");
+        });
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"", "  "})
     void shouldReturnTheNumberOfErrorsAs_numberOfErrors_InferiorOrEqualTo5(String input) {
+        assertSoftly(softly -> {
+            softly.assertThat(testInfo.getDisplayName()).isEqualTo("Should return the number of errors as numberOfErrors inferior or equal to 5 (String)");
+        });
     }
 
     @Test
     void shouldReturnTheNumberOfErrorsAs_numberOfErrors_InferiorOrEqualTo15() {
+        assertSoftly(softly -> {
+            softly.assertThat(testInfo.getDisplayName()).isEqualTo("Should return the number of errors as numberOfErrors inferior or equal to 15");
+        });
     }
 
-    private void methodNotAnnotatedWithTestOrParameterizedTest() {
-    }
+    private void methodNotAnnotatedWithTestOrParameterizedTest() {}
 }
