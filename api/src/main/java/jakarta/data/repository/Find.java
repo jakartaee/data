7a59a30b9bdb7f768fb890adb933d2e68483737a
Otavio Santana <otaviopolianasantana@gmail.com>
2024-02-09 05:04:49 +0000

    docs: update find annotation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/main/java/jakarta/data/repository/Find.java
+++ api/src/main/java/jakarta/data/repository/Find.java
@@ -24,6 +24,30 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+/**
+ * <p>Annotates a repository method that is search entities.</p>
+ *
+ * <p>The {@code Find} annotation indicates that the annotated repository method requests will execute a query by match parameter.
+ * Thus, the types and names of the method parameters exactly match the types and names of the corresponding fields of the entity. Also,
+ * there's no special naming convention for the @Find methods—they may be named arbitrarily, and their names encode no semantics.
+ * </p>
+ * <p>For example, consider an interface representing a garage:</p>
+ * <pre>
+ * {@code @Repository}
+ * interface Garage {
+ *     {@code @Find}
+ *     List<Car> getCarWithModel({@code @By("model"} String model);
+ * }
+ * </pre>
+ * <p>The {@code @Find} annotation can be used to indicate that the {@code getCarWithModel(model)} method is responsible for searching
+ * a list of {@code Car} based at the model in parameter into a database.
+ * </p>
+ *
+ * <p>If this annotation is combined with other operation annotations (e.g., {@code @Update}, {@code @Delete},
+ *  {@code @Save}), it will throw an {@link UnsupportedOperationException} because only one operation type can be specified.
+ *  A Jakarta Data provider implementation must detect (and report) this error at compile time or at runtime.</p>
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
