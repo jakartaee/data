9872421fd80d4e8b15ac4a1c4e6b728ea836a121
Otavio Santana <otaviopolianasantana@gmail.com>
2024-02-11 06:40:33 +0000

    docs: create package info
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ api/src/main/java/jakarta/data/dao/package-info.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+/**
+ * Provides interfaces and classes for Data Access Objects (DAOs) in the Jakarta Data API.
+ * <p>
+ * The DAO package contains abstractions and utilities for working with data access objects,
+ * facilitating the interaction between Java application code and the underlying data storage.
+ * </p>
+ * <p>
+ * This package includes various interfaces and classes that define common patterns and practices
+ * for implementing data access logic in Jakarta EE applications, promoting maintainability,
+ * scalability and portability.
+ * </p>
+ */
+package jakarta.data.dao;
