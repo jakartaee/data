86efda47c317c809bd9d7b1509e5e007b4c40381
Otavio Santana <otaviopolianasantana@gmail.com>
2024-04-08 17:32:05 +0100

    feat: create offsetbasedpage request
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/main/java/jakarta/data/page/OffsetBasedPageRequest.java
+++ api/src/main/java/jakarta/data/page/OffsetBasedPageRequest.java
@@ -1,2 +1,39 @@
-package jakarta.data.page;public interface OffsetBasedPageRequest {
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+package jakarta.data.page;
+
+public interface OffsetBasedPageRequest extends PageRequest {
+
+
+    /**
+     * <p>Creates a new page request with the same pagination information,
+     * but with the specified page number.</p>
+     *
+     * @param pageNumber The page number
+     * @return a new instance of {@code PageRequest}.
+     *         This method never returns {@code null}.
+     */
+    PageRequest page(long pageNumber);
+
+    /**
+     * Returns the page to be returned.
+     *
+     * @return the page to be returned.
+     */
+    long page();
 }
