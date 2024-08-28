4a200aede6606cfdd739bb98495bf449dd053796
Otavio Santana <otaviopolianasantana@gmail.com>
2024-08-28 20:30:37 +0100

    chore: update suggestion to entity test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/src/main/java/ee/jakarta/tck/data/standalone/entity/EntityTests.java
+++ tck/src/main/java/ee/jakarta/tck/data/standalone/entity/EntityTests.java
@@ -2354,20 +2354,7 @@ public class EntityTests {
     public void testUpdateQueryWithoutWhereClause() {
         // Ensure there is no data left over from other tests:
 
-        try {
-            shared.removeAll();
-        } catch (UnsupportedOperationException x) {
-            if (type.isKeywordSupportAtOrBelow(DatabaseType.GRAPH) &&
-                TestProperty.delay.isSet()) {
-                // NoSQL databases with eventual consistency might not be capable
-                // of counting removed entities.
-                // Use alternative approach for ensuring no data is present:
-                boxes.deleteAll(boxes.findAll().toList());
-            } else {
-                throw x;
-            }
-        }
-
+        shared.removeAll();
         TestPropertyUtility.waitForEventualConsistency();
 
         boxes.saveAll(List.of(Box.of("TestUpdateQueryWithoutWhereClause-01", 125, 117, 44),
@@ -2409,21 +2396,11 @@ public class EntityTests {
             assertEquals(120, b3.height); // increased by factor of 2
         }
 
-        try {
-            var removeAllResult = shared.removeAll();
+       var removeAllResult = shared.removeAll();
+        if (!type.isKeywordSupportAtOrBelow(DatabaseType.GRAPH) && !TestProperty.delay.isSet()) {
+            // NoSQL databases might not be capable of arithmetic in updates. Skip this check.
             assertEquals(3, removeAllResult);
-        } catch (UnsupportedOperationException x) {
-            if (type.isKeywordSupportAtOrBelow(DatabaseType.GRAPH) &&
-                TestProperty.delay.isSet()) {
-                // NoSQL databases with eventual consistency might not be capable
-                // of counting removed entities.
-                // Use alternative approach for removing entities.
-                boxes.deleteAll(boxes.findAll().toList());
-            } else {
-                throw x;
-            }
         }
-
         TestPropertyUtility.waitForEventualConsistency();
 
         try {
