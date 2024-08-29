e7d2baa8a6eb6373f9e2bf881d479443a717e5ce
Otávio Santana <otaviopolianasantana@gmail.com>
2024-08-29 15:40:16 +0100

    Update tck/src/main/java/ee/jakarta/tck/data/standalone/entity/EntityTests.java
    Co-authored-by: Nathan Rauh <nathan.rauh@us.ibm.com>

--- tck/src/main/java/ee/jakarta/tck/data/standalone/entity/EntityTests.java
+++ tck/src/main/java/ee/jakarta/tck/data/standalone/entity/EntityTests.java
@@ -2357,7 +2357,7 @@ public class EntityTests {
         try {
             shared.removeAll();
         } catch (UnsupportedOperationException x) {
-            if (type.isKeywordSupportAtOrBelow(DatabaseType.GRAPH)) {
+            if (type.isKeywordSupportAtOrBelow(DatabaseType.GRAPH) && TestProperty.delay.isSet()) {
                 // NoSQL databases with eventual consistency might not be capable
                 // of counting removed entities.
                 // Use alternative approach for ensuring no data is present:
