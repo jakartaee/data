5a4f5159256f277833281f36958352175a8d023b
Otavio Santana <otaviopolianasantana@gmail.com>
2025-04-24 08:19:23 +0100

    test: create attribute record test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ api/src/test/java/jakarta/data/metamodel/impl/AttributeRecordTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package jakarta.data.metamodel.impl;
+
+import jakarta.data.metamodel.Attribute;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AttributeRecordTest {
+
+    interface _SimpleEntity {
+        String ID = "id";
+        String DESCRIPTION = "description";
+
+        Attribute<SimpleEntity> id = new AttributeRecord<>(ID);
+        Attribute<SimpleEntity> description = new AttributeRecord<>(DESCRIPTION);
+    }
+
+    // A simple test entity
+    static class SimpleEntity {
+        String id;
+        String description;
+    }
+
+    @Test
+    @DisplayName("should create attribute record with name and validate interface type")
+    void shouldCreateAttributeRecordWithName() {
+        var attribute = new AttributeRecord<>("id");
+
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(attribute.name()).isEqualTo("id");
+            soft.assertThat(attribute).isInstanceOf(Attribute.class);
+            soft.assertThat(attribute.name()).isNotBlank();
+        });
+    }
+
+    @Test
+    @DisplayName("should support equals and hashCode for same name")
+    void shouldSupportEqualsAndHashCode() {
+        var id1 = new AttributeRecord<>("id");
+        var id2 = new AttributeRecord<>("id");
+
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(id1).isEqualTo(id2);
+            soft.assertThat(id1.hashCode()).isEqualTo(id2.hashCode());
+        });
+    }
+
+    @Test
+    @DisplayName("should not be equal if names differ")
+    void shouldNotEqualDifferentNames() {
+        AttributeRecord<SimpleEntity> id = new AttributeRecord<>("id");
+        AttributeRecord<SimpleEntity> desc = new AttributeRecord<>("description");
+
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(id).isNotEqualTo(desc);
+        });
+    }
+
+    @Test
+    @DisplayName("should throw NullPointerException when name is null")
+    void shouldThrowIfNameIsNull() {
+        org.junit.jupiter.api.Assertions.assertThrows(NullPointerException.class, () -> {
+            new AttributeRecord<>(null);
+        });
+    }
+}
