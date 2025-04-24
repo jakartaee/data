4e17a11bf46d0f81562e9553224a7d54cf7d8e7a
Otavio Santana <otaviopolianasantana@gmail.com>
2025-04-24 08:22:02 +0100

    test: include text attribute record
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/test/java/jakarta/data/metamodel/impl/TextAttributeRecordTest.java
+++ api/src/test/java/jakarta/data/metamodel/impl/TextAttributeRecordTest.java
@@ -17,8 +17,61 @@
  */
 package jakarta.data.metamodel.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import jakarta.data.metamodel.TextAttribute;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class TextAttributeRecordTest {
 
+
+    // A mock static metamodel for tests
+    interface _SimpleEntity {
+        String ID = "id";
+        String NAME = "name";
+
+        TextAttribute<SimpleEntity> id = new TextAttributeRecord<>(ID);
+        TextAttribute<SimpleEntity> name = new TextAttributeRecord<>(NAME);
+    }
+
+    // A simple test entity
+    static class SimpleEntity {
+        String id;
+        String name;
+    }
+
+    @Test
+    @DisplayName("should create text attribute record and validate interface type")
+    void shouldCreateTextAttributeRecord() {
+        var attribute = new TextAttributeRecord<SimpleEntity>("name");
+
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(attribute.name()).isEqualTo("name");
+            soft.assertThat(attribute).isInstanceOf(TextAttribute.class);
+            soft.assertThat(attribute.name()).isNotBlank();
+        });
+    }
+
+    @Test
+    @DisplayName("should support equals and hashCode for same name")
+    void shouldSupportEqualsAndHashCode() {
+        var name1 = new TextAttributeRecord<SimpleEntity>("name");
+        var name2 = new TextAttributeRecord<SimpleEntity>("name");
+
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(name1).isEqualTo(name2);
+            soft.assertThat(name1.hashCode()).isEqualTo(name2.hashCode());
+        });
+    }
+
+    @Test
+    @DisplayName("should not be equal if names differ")
+    void shouldNotEqualDifferentNames() {
+        var idAttribute = new TextAttributeRecord<SimpleEntity>("id");
+        var nameAttribute = new TextAttributeRecord<SimpleEntity>("name");
+
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(idAttribute).isNotEqualTo(nameAttribute);
+        });
+    }
 }
