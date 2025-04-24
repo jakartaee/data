a24e531b3298456c54abab6a3d7b307d8a1ddba3
Otavio Santana <otaviopolianasantana@gmail.com>
2025-04-24 19:51:05 +0100

    test: generate comparable expression test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/test/java/jakarta/data/metamodel/ComparableExpressionTest.java
+++ api/src/test/java/jakarta/data/metamodel/ComparableExpressionTest.java
@@ -17,8 +17,71 @@
  */
 package jakarta.data.metamodel;
 
-import static org.junit.jupiter.api.Assertions.*;
+import jakarta.data.metamodel.constraint.*;
+import jakarta.data.metamodel.restrict.BasicRestriction;
+import jakarta.data.metamodel.restrict.Restriction;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class ComparableExpressionTest {
 
+    static class Person {
+        Integer age;
+    }
+
+    // Mock metamodel
+    interface _Person {
+        String AGE = "age";
+        ComparableExpression<Person, Integer> age =
+                new ComparableAttributeRecord<>(Person.class, AGE);
+    }
+
+    @Test
+    @DisplayName("should create Restriction with between values")
+    void shouldCreateBetweenValueRestriction() {
+        var restriction = _Person.age.between(18, 30);
+
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(restriction).isInstanceOf(BasicRestriction.class);
+            soft.assertThat(((BasicRestriction<?, ?>) restriction).constraint()).isEqualTo(Between.bounds(18, 30));
+        });
+    }
+
+    @Test
+    @DisplayName("should create Restriction with greaterThan value")
+    void shouldCreateGreaterThanRestriction() {
+        var restriction = _Person.age.greaterThan(21);
+
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(restriction).isInstanceOf(BasicRestriction.class);
+            soft.assertThat(((BasicRestriction<?, ?>) restriction).constraint()).isEqualTo(GreaterThan.bound(21));
+        });
+    }
+
+    @Test
+    @DisplayName("should create Restriction with greaterThanEqual value")
+    void shouldCreateGreaterThanEqualRestriction() {
+        var restriction = _Person.age.greaterThanEqual(65);
+
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(restriction).isInstanceOf(BasicRestriction.class);
+            soft.assertThat(((BasicRestriction<?, ?>) restriction).constraint()).isEqualTo(GreaterThanOrEqual.min(65));
+        });
+    }
+
+    @Test
+    @DisplayName("should create between expression restriction with min and max expressions")
+    void shouldCreateBetweenExpressionRestriction() {
+        var min = _Person.age;
+        var max = _Person.age;
+
+        var restriction = _Person.age.between(min, max);
+
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(restriction).isInstanceOf(BasicRestriction.class);
+            soft.assertThat(((BasicRestriction<?, ?>) restriction).constraint())
+                    .isEqualTo(Between.bounds(min, max));
+        });
+    }
 }
