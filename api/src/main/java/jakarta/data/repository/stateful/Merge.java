74fddfafdc09ede1ae63b7eb29f98161557b6eb5
Gavin King <gavin@hibernate.org>
2025-06-05 09:05:22 +0200

    make the naggy bot happy
    see #470

--- api/src/main/java/jakarta/data/repository/stateful/Merge.java
+++ api/src/main/java/jakarta/data/repository/stateful/Merge.java
@@ -17,7 +17,11 @@
  */
 package jakarta.data.repository.stateful;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * <p>Lifecycle annotation for repository methods of stateful repositories
