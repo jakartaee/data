9ccd51db8535776ad95275351f853e821cac1507
Otavio Santana <otaviopolianasantana@gmail.com>
2025-06-08 18:43:25 +0100

    docs: generate documentation to constraint
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- api/src/main/java/jakarta/data/constraint/Constraint.java
+++ api/src/main/java/jakarta/data/constraint/Constraint.java
@@ -37,30 +37,119 @@ import java.util.Set;
  */
 public interface Constraint<V> {
 
+    /**
+     * Returns a constraint that represents the logical negation of this constraint.
+     *
+     * <pre>{@code
+     * List<Product> results = products.findAll(
+     *     _Product.type.equalTo(ProductType.AUTOMOTIVE).negate()
+     * );
+     * }</pre>
+     *
+     * @return a constraint representing the logical negation
+     */
     Constraint<V> negate();
 
+    /**
+     * Creates a constraint that checks if an attribute is equal to the specified value.
+     *
+     * <pre>{@code
+     * List<Product> results = products.findAll(
+     *     _Product.type.equalTo(ProductType.AUTOMOTIVE)
+     * );
+     * }</pre>
+     *
+     * @param value the value to compare against
+     * @param <V> the value type
+     * @return a constraint that checks for equality with the given value
+     */
     static <V> EqualTo<V> equalTo(V value) {
         return EqualTo.value(value);
     }
 
+    /**
+     * Creates a constraint that checks if an attribute is not equal to the specified value.
+     *
+     * <pre>{@code
+     * List<Product> results = products.findAll(
+     *     _Product.type.notEqualTo(ProductType.AUTOMOTIVE)
+     * );
+     * }</pre>
+     *
+     * @param value the value to compare against
+     * @param <V> the value type
+     * @return a constraint that checks for inequality with the given value
+     */
     static <V> NotEqualTo<V> notEqualTo(V value) {
         return NotEqualTo.value(value);
     }
 
+    /**
+     * Creates a constraint that checks if an attribute's value is in the specified list.
+     *
+     * <pre>{@code
+     * List<Product> results = products.findAll(
+     *     _Product.type.in(ProductType.AUTOMOTIVE, ProductType.ELECTRONICS)
+     * );
+     * }</pre>
+     *
+     * @param values the values to match against
+     * @param <V> the value type
+     * @return a constraint that checks if the attribute matches one of the specified values
+     */
     @SafeVarargs
     static <V> In<V> in(V... values) {
         return In.values(values);
     }
 
+    /**
+     * Creates a constraint that checks if an attribute's value is in the specified set.
+     *
+     * <pre>{@code
+     * List<Product> results = products.findAll(
+     *     _Product.type.in(Set.of(ProductType.AUTOMOTIVE, ProductType.ELECTRONICS))
+     * );
+     * }</pre>
+     *
+     * @param values the values to match against
+     * @param <V> the value type
+     * @return a constraint that checks if the attribute matches one of the specified values
+     */
     static <V> In<V> in(Set<V> values) {
         return In.values(values);
     }
 
+    /**
+     * Creates a constraint that checks if an attribute's value is not in the specified list.
+     *
+     * <pre>{@code
+     * List<Product> results = products.findAll(
+     *     _Product.type.notIn(ProductType.AUTOMOTIVE, ProductType.ELECTRONICS)
+     * );
+     * }</pre>
+     *
+     * @param values the values the attribute must not match
+     * @param <V> the value type
+     * @return a constraint that checks if the attribute does not match any of the specified values
+     */
     @SafeVarargs
     static <V> NotIn<V> notIn(V... values) {
         return NotIn.values(values);
     }
 
+    /**
+     * Creates a constraint that checks if an attribute's value is not in the specified set.
+     *
+     * <pre>{@code
+     * List<Product> results = products.findAll(
+     *     _Product.type.notIn(Set.of(ProductType.AUTOMOTIVE, ProductType.ELECTRONICS))
+     * );
+     * }</pre>
+     *
+     * @param values the values the attribute must not match
+     * @param <V> the value type
+     * @return a constraint that checks if the attribute does not match any of the specified values
+     */
     static <V> NotIn<V> notIn(Set<V> values) {
         return NotIn.values(values);
     }
@@ -89,34 +178,138 @@ public interface Constraint<V> {
         return NotLike.pattern(pattern, charWildcard, stringWildcard, escape);
     }
 
+    /**
+     * Creates a constraint that checks if an attribute's value is {@code null}.
+     *
+     * <pre>{@code
+     * List<Product> results = products.findAll(
+     *     _Product.description.isNull()
+     * );
+     * }</pre>
+     *
+     * @param <V> the value type
+     * @return a constraint that checks if the attribute is {@code null}
+     */
     static <V> Null<V> isNull() {
         return Null.instance();
     }
 
+    /**
+     * Creates a constraint that checks if an attribute's value is not {@code null}.
+     *
+     * <pre>{@code
+     * List<Product> results = products.findAll(
+     *     _Product.description.notNull()
+     * );
+     * }</pre>
+     *
+     * @param <V> the value type
+     * @return a constraint that checks if the attribute is not {@code null}
+     */
     static <V> NotNull<V> notNull() {
         return NotNull.instance();
     }
 
+    /**
+     * Creates a constraint that checks if an attribute's value is greater than the given bound.
+     *
+     * <pre>{@code
+     * List<Product> results = products.findAll(
+     *     _Product.price.greaterThan(100.0)
+     * );
+     * }</pre>
+     *
+     * @param <V> the value type, must be {@link Comparable}
+     * @param bound the lower exclusive bound
+     * @return a constraint representing {@code greaterThan}
+     */
     static <V extends Comparable<?>> GreaterThan<V> greaterThan(V bound) {
         return GreaterThan.bound(bound);
     }
 
+    /**
+     * Creates a constraint that checks if an attribute's value is less than the given bound.
+     *
+     * <pre>{@code
+     * List<Product> results = products.findAll(
+     *     _Product.price.lessThan(100.0)
+     * );
+     * }</pre>
+     *
+     * @param <V> the value type, must be {@link Comparable}
+     * @param bound the upper exclusive bound
+     * @return a constraint representing {@code lessThan}
+     */
     static <V extends Comparable<?>> LessThan<V> lessThan(V bound) {
         return LessThan.bound(bound);
     }
 
+    /**
+     * Creates a constraint that checks if an attribute's value is greater than or equal to the given bound.
+     *
+     * <pre>{@code
+     * List<Product> results = products.findAll(
+     *     _Product.price.greaterThanOrEqual(100.0)
+     * );
+     * }</pre>
+     *
+     * @param <V> the value type, must be {@link Comparable}
+     * @param bound the minimum inclusive bound
+     * @return a constraint representing {@code greaterThanOrEqual}
+     */
     static <V extends Comparable<?>> GreaterThanOrEqual<V> greaterThanOrEqual(V bound) {
         return GreaterThanOrEqual.min(bound);
     }
 
+    /**
+     * Creates a constraint that checks if an attribute's value is less than or equal to the given bound.
+     *
+     * <pre>{@code
+     * List<Product> results = products.findAll(
+     *     _Product.price.lessThanOrEqual(100.0)
+     * );
+     * }</pre>
+     *
+     * @param <V> the value type, must be {@link Comparable}
+     * @param bound the maximum inclusive bound
+     * @return a constraint representing {@code lessThanOrEqual}
+     */
     static <V extends Comparable<?>> LessThanOrEqual<V> lessThanOrEqual(V bound) {
         return LessThanOrEqual.max(bound);
     }
 
+    /**
+     * Creates a constraint that checks if an attribute's value is between the given lower and upper bounds, inclusive.
+     *
+     * <pre>{@code
+     * List<Product> results = products.findAll(
+     *     _Product.price.between(10.0, 100.0)
+     * );
+     * }</pre>
+     *
+     * @param <V> the value type, must be {@link Comparable}
+     * @param lowerBound the inclusive lower bound
+     * @param upperBound the inclusive upper bound
+     * @return a constraint representing {@code between}
+     */
     static <V extends Comparable<?>> Between<V> between(V lowerBound, V upperBound) {
         return Between.bounds(lowerBound, upperBound);
     }
 
+    /**
+     * Creates a constraint that checks if an attribute's value is outside the given bounds.
+     *
+     * <pre>{@code
+     * List<Product> results = products.findAll(
+     *     _Product.price.notBetween(10.0, 100.0)
+     * );
+     * }</pre>
+     *
+     * @param <V> the value type, must be {@link Comparable}
+     * @param lowerBound the inclusive lower bound
+     * @param upperBound the inclusive upper bound
+     * @return a constraint representing {@code notBetween}
+     */
     static <V extends Comparable<?>> NotBetween<V> notBetween(V lowerBound, V upperBound) {
         return NotBetween.bounds(lowerBound, upperBound);
     }
