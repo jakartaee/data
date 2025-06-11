d14e4825e9ffb448c1cdefbbd9e98d70f9a89b84
Otávio Santana <otaviopolianasantana@gmail.com>
2025-06-11 14:43:27 +0100

    Update stateful/src/main/java/jakarta/data/stateful/Merge.java
    Co-authored-by: Nathan Rauh <nathan.rauh@us.ibm.com>

--- stateful/src/main/java/jakarta/data/stateful/Merge.java
+++ stateful/src/main/java/jakarta/data/stateful/Merge.java
@@ -53,12 +53,13 @@ import java.lang.annotation.Target;
  * must be distinct from the argument entity, but which holds the same
  * persistent state.
  * </p>
- * <p>Every Jakarta Data provider which supports stateful repositories is
+ * <p>A Jakarta Data provider that is capable of returning
+ * {@code jakarta.persistence.EntityManager} from a resource accessor method is
  * required to accept a {@code Merge} method which conforms to this signature.
+ * A {@code Merge} method that is not accepted must raise
+ * {@link UnsupportedOperationException} or be rejected at compile time.
  * Application of the {@code Merge} annotation to a method with any other
- * signature is not portable between Jakarta Data providers. Furthermore,
- * support for stateful repositories is optional. A Jakarta Data provider is
- * not required to support stateful repositories.
+ * signature is not portable between Jakarta Data providers.
  * </p>
  * <p>A given method of a repository interface may have at most one
  * {@code @Find} annotation, {@code @Query} annotation, {@code @Merge}
