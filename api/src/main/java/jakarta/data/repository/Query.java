a5395d3480af141772d7c6c8ec9b1ac14020ab7f
Gavin King <gavin@hibernate.org>
2025-06-25 12:19:11 +0200

    Update spec to reflect addition of Constraint and @In

--- api/src/main/java/jakarta/data/repository/Query.java
+++ api/src/main/java/jakarta/data/repository/Query.java
@@ -99,8 +99,8 @@ import java.lang.annotation.Target;
  *     of the query,</li>
  * <li>have exactly the same type and position within the parameter list of
  *     the method as a positional parameter of the query, or</li>
- * <li>be of type {@link Limit}, {@link Order}, {@link PageRequest},
- *     {@link Restriction}, or {@link Sort}.</li>
+ * <li>be of type {@link Limit}, {@link Restriction}, {@link Sort}, {@link Order},
+ *     or {@link PageRequest}.</li>
  * </ul>
  *
  * <p>The {@link Param} annotation associates a method parameter with a named
