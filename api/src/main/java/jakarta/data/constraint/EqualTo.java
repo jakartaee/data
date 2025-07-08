7bb682cf159ca8acf26e3598c228702ea2d65677
Nathan Rauh <nathan.rauh@us.ibm.com>
2025-07-08 10:46:04 -0500

    Javadoc for EqualTo and NotEqualTo constraints

--- api/src/main/java/jakarta/data/constraint/EqualTo.java
+++ api/src/main/java/jakarta/data/constraint/EqualTo.java
@@ -17,12 +17,93 @@
  */
 package jakarta.data.constraint;
 
-
 import jakarta.data.expression.Expression;
 import jakarta.data.messages.Messages;
+import jakarta.data.metamodel.Attribute;
+import jakarta.data.repository.Is;
+import jakarta.data.restrict.Restriction;
 import jakarta.data.spi.expression.literal.Literal;
+
+/**
+ * <p>A constraint that requires equality.</p>
+ *
+ * <p>A parameter-based repository method can impose a constraint on an
+ * entity attribute by defining a method parameter that is of type
+ * {@code EqualTo} or is annotated {@link Is @Is(EqualTo.class)} and is
+ * of the same type or a subtype of the entity attribute. The equality
+ * constraint is also the default when the {@code @Is} annotation is omitted.
+ * For example,</p>
+ *
+ * <pre>
+ * &#64;Find
+ * List&lt;Car&gt; fromManufacturer(&#64;By(_Car.MAKE) EqualTo&lt;String&gt; manufacturer);
+ *
+ * &#64;Find
+ * List&lt;Car&gt; ofMakeAndModel(&#64;By(_Car.MAKE) &#64;Is(EqualTo.class) String manufacturer,
+ *                          &#64;By(_Car.MODEL) &#64;Is(EqualTo.class) String model,
+ *                          Order&lt;Car&gt; sorts);
+ *
+ * &#64;Find
+ * List&lt;Car&gt; ofMakeAndModelAndYear(&#64;By(_Car.MAKE) String manufacturer,
+ *                                 &#64;By(_Car.MODEL) String model,
+ *                                 &#64;By(_Car.YEAR) int modelYear,
+ *                                 Order&lt;Car&gt; sorts);
+ * ...
+ *
+ * found = cars.fromManufacturer(EqualTo.value("Jakarta Motors"));
+ *
+ * found = cars.ofMakeAndModel("Jakarta Motors",
+ *                             "J-150",
+ *                             Order.by(_Car.price.desc()));
+ *
+ * found = cars.ofMakeAndModelAndYear("Jakarta Motors",
+ *                                    "J-150",
+ *                                    2025,
+ *                                    Order.by(_Car.price.desc()));
+ * </pre>
+ *
+ * <p>Repository methods can also accept {@code EqualTo} constraints at
+ * run time in the form of a {@link Restriction} on an {@link Expression}.
+ * For example,</p>
+ *
+ * <pre>
+ * &#64;Find
+ * List&lt;Car&gt; searchAll(Restriction&lt;Car&gt; restrict, Order&lt;Car&gt; sorts);
+ *
+ * ...
+ *
+ * found = cars.searchAll(Restrict.all(_Car.make.equalTo("Jakarta Motors"),
+ *                                     _Car.model.equalTo("J-150")),
+ *                        Order.by(_Car.year.desc(),
+ *                                 _Car.price.asc());
+ * </pre>
+ *
+ * <p>The {@linkplain Attribute entity and static metamodel} for the code
+ * examples within this class are shown in the {@link Attribute} Javadoc.
+ * </p>
+ *
+ * @param <V> type of the entity attribute or a subtype or primitive wrapper
+ *            type for the entity attribute.
+ * @since 1.1
+ */
 public interface EqualTo<V> extends Constraint<V> {
 
+    /**
+     * <p>Requires that the constraint target equal the value to which the
+     * given {@code expression} evaluates. For example,</p>
+     *
+     * <pre>
+     * found = cars.fromManufacturer(
+     *                 EqualTo.expression(_Car.model.left(_Car.make.length())));
+     * </pre>
+     *
+     * @param <V>        type of the entity attribute or a subtype or primitive
+     *                   wrapper type for the entity attribute.
+     * @param expression an expression that evaluates to a value against which
+     *                   the constraint target is compared.
+     * @return an {@code EqualTo} constraint.
+     * @throws NullPointerException if the expression is {@code null}.
+     */
     static <V> EqualTo<V> expression(Expression<?, V> expression) {
         if (expression == null) {
             throw new NullPointerException(
@@ -32,6 +113,20 @@ public interface EqualTo<V> extends Constraint<V> {
         return new EqualToRecord<>(expression);
     }
 
+    /**
+     * <p>Requires that the constraint target equal the given value. For
+     * example,</p>
+     *
+     * <pre>
+     * found = cars.fromManufacturer(EqualTo.value("Jakarta Motors"));
+     * </pre>
+     *
+     * @param <V>   type of the entity attribute or a subtype or primitive
+     *              wrapper type for the entity attribute.
+     * @param value a value against which the constraint target is compared.
+     * @return an {@code EqualTo} constraint.
+     * @throws NullPointerException if the value is {@code null}.
+     */
     static <V> EqualTo<V> value(V value) {
         if (value == null) {
             throw new NullPointerException(
@@ -41,5 +136,11 @@ public interface EqualTo<V> extends Constraint<V> {
         return new EqualToRecord<>(Literal.of(value));
     }
 
+    /**
+     * <p>An expression that evaluates to the value against which the
+     * constraint target is compared.</p>
+     *
+     * @return an expression representing the value.
+     */
     Expression<?, V> expression();
 }
