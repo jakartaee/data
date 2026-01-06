9db1a8ef476164b5d8c5f229cb7427a0ef2214d6
Otavio Santana <otaviopolianasantana@gmail.com>
2026-01-06 17:52:27 +0000

    feat: introduce AbstractSupplier and AbstractListSupplier with VehicleListSupplier implementation

---
+++ tck/src/main/java/ee/jakarta/tck/data/standalone/entity/AbstractSupplier.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ * <p>
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * <p>
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ * <p>
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+package ee.jakarta.tck.data.standalone.entity;
+
+import net.datafaker.Faker;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+abstract class AbstractSupplier<T> implements Supplier<T>, ArgumentsProvider {
+
+    private static final Faker FAKER = new Faker();
+
+    /**
+     * Provides access to the shared {@link Faker} instance for generating fake data.
+     *
+     * @return the shared {@link Faker} instance
+     */
+    protected Faker faker() {
+        return FAKER;
+    }
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+        return Stream.of(Arguments.of(get()));
+    }
+}
