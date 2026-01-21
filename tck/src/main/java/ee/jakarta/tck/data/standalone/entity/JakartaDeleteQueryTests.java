2055b4a2dffbab125185ad3686dbd19063fcaec4
Otávio Santana <otaviopolianasantana@gmail.com>
2026-01-21 18:37:32 +0000

    Update tck/src/main/java/ee/jakarta/tck/data/standalone/entity/JakartaDeleteQueryTests.java
    Co-authored-by: Nathan Rauh <nathan.rauh@us.ibm.com>

--- tck/src/main/java/ee/jakarta/tck/data/standalone/entity/JakartaDeleteQueryTests.java
+++ tck/src/main/java/ee/jakarta/tck/data/standalone/entity/JakartaDeleteQueryTests.java
@@ -258,7 +258,7 @@ public class JakartaDeleteQueryTests {
     }
 
     @DisplayName("should test OR")
-    @Assertion(id = "1318",
+    @Assertion(id = "458",
             strategy = "delete using OR condition")
     void shouldDeleteUsingOrCondition() {
 
