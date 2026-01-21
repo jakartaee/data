e77452862f1b7ed4f5a3b20a47d800830cfc2cc2
Otavio Santana <otaviopolianasantana@gmail.com>
2026-01-21 07:58:38 +0000

    refactor: update display name and remove unused delete query tests in JakartaQueryTests

--- tck/src/main/java/ee/jakarta/tck/data/standalone/entity/JakartaQueryTests.java
+++ tck/src/main/java/ee/jakarta/tck/data/standalone/entity/JakartaQueryTests.java
@@ -19,16 +19,13 @@ import ee.jakarta.tck.data.framework.junit.anno.AnyEntity;
 import ee.jakarta.tck.data.framework.junit.anno.Standalone;
 import ee.jakarta.tck.data.framework.utilities.DatabaseType;
 import ee.jakarta.tck.data.framework.utilities.TestProperty;
-import ee.jakarta.tck.data.framework.utilities.TestPropertyUtility;
 import jakarta.inject.Inject;
 import org.assertj.core.api.Assertions;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import ee.jakarta.tck.data.framework.junit.anno.Assertion;
 import ee.jakarta.tck.data.framework.read.only.FruitPopulator;
 
@@ -39,7 +36,7 @@ import java.util.logging.Logger;
 
 @Standalone
 @AnyEntity
-@DisplayName("the Jakarta Query Tests integration")
+@DisplayName("Jakarta Data integration with Jakarta Common Query Language for select operations")
 public class JakartaQueryTests {
 
     public static final Logger log = Logger.getLogger(JakartaQueryTests.class.getCanonicalName());
@@ -64,9 +61,6 @@ public class JakartaQueryTests {
         populator.populate(fruitRepository);
     }
 
-    @Nested
-    @DisplayName("When execute the select statement")
-    class WhendSelectQuery {
     @DisplayName("should find all entities as stream")
     @Assertion(id = "1318",
             strategy = "Execute a repository query using the " +
@@ -558,232 +552,6 @@ public class JakartaQueryTests {
                 // Column and Key-Value databases might not be capable of filtering + sorting on non-key attributes.
             } else {
                 throw exp;
-            }
-        }
-    }
-    }
-
-    @Nested
-    @DisplayName("When execute delete query")
-    class WhenDeleteQuery {
-
-
-        @AfterEach
-        public void cleanup() {
-            fruitRepository.deleteAll();
-            TestPropertyUtility.waitForEventualConsistency();
-        }
-
-        @DisplayName("should delete all entities")
-        @Assertion(id = "1318",
-                strategy = "Execute the delete all queries, wait for eventual consistency and verify if all entities are deleted")
-        void shouldDeleteAllEntities() {
-
-            try {
-                fruitRepository.deleteAll();
-                TestPropertyUtility.waitForEventualConsistency();
-
-                Assertions.assertThat(fruitRepository.findAll().toList()).isEmpty();
-            } catch (UnsupportedOperationException exp) {
-                if (type.isKeywordSupportAtOrBelow(DatabaseType.COLUMN)) {
-                    // Column and Key-Value databases might not be capable of delete all the entities
-                } else {
-                    throw exp;
-                }
-            }
-        }
-
-        @DisplayName("should delete entities using equals condition")
-        @Assertion(id = "1318",
-                strategy = "delete by name equals, verify if entity is deleted")
-        void shouldDeleteEq() {
-            try {
-                Fruit fruit = fruits.getFirst();
-                fruitRepository.deleteByName(fruit.getName());
-                TestPropertyUtility.waitForEventualConsistency();
-
-                List<Fruit> result = fruitRepository.findAll().toList();
-                Assertions.assertThat(result)
-                        .allMatch(f -> !fruit.getName().equals(f.getName()));
-            } catch (UnsupportedOperationException exp) {
-                if (type.isKeywordSupportAtOrBelow(DatabaseType.COLUMN)) {
-                    // Column and Key-Value databases might not be capable deleting by attribute that is not a key.
-                } else {
-                    throw exp;
-                }
-            }
-        }
-
-        @DisplayName("should delete not equals condition")
-        @Assertion(id = "1318",
-                strategy = "delete by name not equals, verify if entity is deleted")
-        void shouldDeleteNeq() {
-            try {
-                Fruit fruit = fruits.getFirst();
-                fruitRepository.deleteByNotEqualsName(fruit.getName());
-                TestPropertyUtility.waitForEventualConsistency();
-
-                List<Fruit> result = fruitRepository.findAll().toList();
-                Assertions.assertThat(result)
-                        .allMatch(f -> fruit.getName().equals(f.getName()));
-            } catch (UnsupportedOperationException exp) {
-                if (type.isKeywordSupportAtOrBelow(DatabaseType.COLUMN)) {
-                    // Column and Key-Value databases might not be capable deleting by attribute that is not a key.
-                } else {
-                    throw exp;
-                }
-            }
-        }
-
-        @DisplayName("should delete greater than condition")
-        @Assertion(id = "1318",
-                strategy = "delete by quantity greater than, verify if entity is deleted")
-        void shouldGt() {
-            try {
-                Fruit fruit = fruits.getFirst();
-                fruitRepository.deleteQuantityGreaterThan(fruit.getQuantity());
-                TestPropertyUtility.waitForEventualConsistency();
-
-                List<Fruit> result = fruitRepository.findAll().toList();
-                Assertions.assertThat(result)
-                        .allMatch(f -> f.getQuantity() <= fruit.getQuantity());
-            } catch (UnsupportedOperationException exp) {
-                if (type.isKeywordSupportAtOrBelow(DatabaseType.COLUMN)) {
-                    // Column and Key-Value databases might not be capable deleting by attribute that is not a key.
-                } else {
-                    throw exp;
-                }
-            }
-        }
-
-        @DisplayName("should delete greater than equals condition")
-        @Assertion(id = "1318",
-                strategy = "delete by quantity greater than equals, verify if entity is deleted")
-        void shouldGte() {
-            try {
-                Fruit fruit = fruits.getFirst();
-                fruitRepository.deleteQuantityGreaterThanEquals(fruit.getQuantity());
-                TestPropertyUtility.waitForEventualConsistency();
-
-                List<Fruit> result = fruitRepository.findAll().toList();
-                Assertions.assertThat(result)
-                        .allMatch(f -> f.getQuantity() < fruit.getQuantity());
-            } catch (UnsupportedOperationException exp) {
-                if (type.isKeywordSupportAtOrBelow(DatabaseType.COLUMN)) {
-                    // Column and Key-Value databases might not be capable deleting by attribute that is not a key.
-                } else {
-                    throw exp;
-                }
-            }
-        }
-
-        @DisplayName("should delete lesser condition")
-        @Assertion(id = "1318",
-                strategy = "delete by quantity lesser, verify if entity is deleted")
-        void shouldLt() {
-            try {
-                Fruit fruit = fruits.getFirst();
-                fruitRepository.deleteLesserThan(fruit.getQuantity());
-                TestPropertyUtility.waitForEventualConsistency();
-
-                List<Fruit> result = fruitRepository.findAll().toList();
-                Assertions.assertThat(result)
-                        .allMatch(f -> f.getQuantity() >= fruit.getQuantity());
-            } catch (UnsupportedOperationException exp) {
-                if (type.isKeywordSupportAtOrBelow(DatabaseType.COLUMN)) {
-                    // Column and Key-Value databases might not be capable deleting by attribute that is not a key.
-                } else {
-                    throw exp;
-                }
-            }
-        }
-
-        @DisplayName("should delete lesser than equals condition")
-        @Assertion(id = "1318",
-                strategy = "delete by quantity lesser than equals, verify if entity is deleted")
-        void shouldLte() {
-            try {
-                Fruit fruit = fruits.getFirst();
-                fruitRepository.deleteQuantityLesserThanEquals(fruit.getQuantity());
-                TestPropertyUtility.waitForEventualConsistency();
-
-                List<Fruit> result = fruitRepository.findAll().toList();
-                Assertions.assertThat(result)
-                        .allMatch(f -> f.getQuantity() > fruit.getQuantity());
-            } catch (UnsupportedOperationException exp) {
-                if (type.isKeywordSupportAtOrBelow(DatabaseType.COLUMN)) {
-                    // Column and Key-Value databases might not be capable deleting by attribute that is not a key.
-                } else {
-                    throw exp;
-                }
-            }
-        }
-
-        @DisplayName("should delete using IN condition")
-        @Assertion(id = "1318",
-                strategy = "delete by name in, verify if entity is deleted")
-        void shouldIn() {
-            try {
-                Fruit fruit = fruits.getFirst();
-                fruitRepository.deleteByNameIn(List.of(fruit.getName(), fruits.get(1).getName()));
-                TestPropertyUtility.waitForEventualConsistency();
-
-                List<Fruit> result = fruitRepository.findAll().toList();
-                Assertions.assertThat(result)
-                        .allMatch(f -> !f.getName().equals(fruit.getName())
-                                ||
-                                !f.getName().equals(fruits.get(1).getName()));
-            } catch (UnsupportedOperationException exp) {
-                if (type.isKeywordSupportAtOrBelow(DatabaseType.COLUMN)) {
-                    // Column and Key-Value databases might not be capable deleting by attribute that is not a key.
-                } else {
-                    throw exp;
-                }
-            }
-        }
-
-        @DisplayName("should test AND")
-        @Assertion(id = "1318",
-                strategy = "delete using AND condition")
-        void shouldDeleteUsingAndCondition() {
-
-            try {
-                Fruit fruit = fruits.getFirst();
-                fruitRepository.deleteByNameAndQuantity(fruit.getName(), fruit.getQuantity());
-                TestPropertyUtility.waitForEventualConsistency();
-                List<Fruit> result = fruitRepository.findAll().toList();
-                Assertions.assertThat(result)
-                        .allMatch(f -> !(f.getName().equals(fruit.getName())
-                                && f.getQuantity().equals(fruit.getQuantity())));
-            } catch (UnsupportedOperationException exp) {
-                if (type.isKeywordSupportAtOrBelow(DatabaseType.COLUMN)) {
-                    // Column and Key-Value databases might not be capable deleting by attribute that is not a key.
-                } else {
-                    throw exp;
-                }
-            }
-        }
-
-        @DisplayName("should test OR")
-        @Assertion(id = "1318",
-                strategy = "delete using OR condition")
-        void shouldDeleteUsingOrCondition() {
-
-            try {
-                Fruit fruit = fruits.getFirst();
-                fruitRepository.deleteByNameOrQuantity(fruit.getName(), fruit.getQuantity());
-                TestPropertyUtility.waitForEventualConsistency();
-                List<Fruit> result = fruitRepository.findAll().toList();
-
-                Assertions.assertThat(result)
-                        .allMatch(f -> !(f.getName().equals(fruit.getName())
-                                || f.getQuantity().equals(fruit.getQuantity())));
-            } catch (UnsupportedOperationException exp) {
-                if (type.isKeywordSupportAtOrBelow(DatabaseType.COLUMN)) {
-                    // Column and Key-Value databases might not be capable deleting by attribute that is not a key.
-                } else {
-                    throw exp;
-                }
             }
         }
     }
