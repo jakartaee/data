c125c049a8ce83c03c8177d8b45b4f2a0d53755a
Nathan Rauh <nathan.rauh@us.ibm.com>
2026-02-10 10:29:05 -0600

    ConstraintTests.testGreaterThanConstraint supplies wrong value

--- tck/src/main/java/ee/jakarta/tck/data/standalone/entity/ConstraintTests.java
+++ tck/src/main/java/ee/jakarta/tck/data/standalone/entity/ConstraintTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ * Copyright (c) 2025,2026 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -215,7 +215,7 @@ public class ConstraintTests {
 
         try {
             List<String> found = countries
-                    .withCapitalBiggerThan(GreaterThan.bound(1000000))
+                    .withCapitalBiggerThan(GreaterThan.bound(5000000))
                             .stream()
                             .map(Country::getCapital)
                             .map(capital -> capital.getName() +
