55b54849cdf6757e6563f5901193855ca786abe2
Nathan Rauh <nathan.rauh@us.ibm.com>
2026-02-10 08:42:25 -0600

    Constraints for True and False

---
+++ api/src/main/java/jakarta/data/constraint/True.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package jakarta.data.constraint;
+
+import jakarta.data.expression.Expression;
+import jakarta.data.metamodel.Attribute;
+import jakarta.data.restrict.Restriction;
+
+/**
+ * <p>A constraint that requires a {@code true} value.</p>
+ *
+ * <p>A parameter-based repository method can impose a constraint on an
+ * entity attribute by defining a method parameter that is of type
+ * {@code True}. For example,</p>
+ *
+ * <pre>{@code
+ * @Find
+ * List<Car> usedVehicles(@By(_Car.MAKE) String make,
+ *                        @By(_Car.MODEL) String model,
+ *                        @By(_Car.PREVIOUSLYOWNED) True preOwned);
+ * ...
+ *
+ * found = cars.usedVehicles("Jakarta Motors",
+ *                           "Jukon",
+ *                           True.instance());
+ * }</pre>
+ *
+ * <p>Repository methods can also accept {@code True} constraints at run time
+ * in the form of a {@link Restriction} on an {@link Expression}. For example,
+ * </p>
+ *
+ * <pre>{@code
+ * @Find
+ * List<Car> searchAll(Restriction<Car> restrict, Order<Car> sorts);
+ *
+ * ...
+ *
+ * found = cars.searchAll(Restrict.all(_Car.make.equalTo("Jakarta Motors"),
+ *                                     _Car.model.equalTo("Jukon"),
+ *                                     _Car.previouslyOwned.isTrue()),
+ *                        Order.by(_Car.year.desc(),
+ *                                 _Car.price.asc()));
+ * }</pre>
+ *
+ * <p>The {@linkplain Attribute entity and static metamodel} for the code
+ * examples within this class are shown in the {@link Attribute} Javadoc.
+ * </p>
+ *
+ * @since 1.1
+ */
+public interface True extends Constraint<Boolean> {
+
+    /**
+     * <p>Requires that the constraint target has a {@code true} value. For
+     * example,</p>
+     *
+     * <pre>{@code
+     *     found = cars.preOwnedVehicles("Jakarta Motors",
+     *                                   "Jukon",
+     *                                   True.instance());
+     * }</pre>
+     *
+     * @return a {@code True} constraint.
+     */
+    static True instance() {
+        return TrueRecord.INSTANCE;
+    }
+}
