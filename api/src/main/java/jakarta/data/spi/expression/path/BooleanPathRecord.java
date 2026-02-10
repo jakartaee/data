2a2603d5c649127e3af47e702328ebd50858a3af
Nathan Rauh <nathan.rauh@us.ibm.com>
2026-02-10 08:47:56 -0600

    BooleanPath

---
+++ api/src/main/java/jakarta/data/spi/expression/path/BooleanPathRecord.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package jakarta.data.spi.expression.path;
+
+import jakarta.data.expression.NavigableExpression;
+import jakarta.data.messages.Messages;
+import jakarta.data.metamodel.BooleanAttribute;
+
+record BooleanPathRecord<T, U>(NavigableExpression<T, U> expression,
+                               BooleanAttribute<U> attribute)
+        implements BooleanPath<T, U> {
+
+    BooleanPathRecord {
+        Messages.requireNonNull(expression, "expression");
+        Messages.requireNonNull(attribute, "attribute");
+    }
+
+    @Override
+    public String toString() {
+        String expr = expression.toString();
+        String attrName = attribute.name();
+        StringBuilder path =
+                new StringBuilder(expr.length() + 1 + attrName.length());
+        path.append(expr).append('.').append(attrName);
+        return path.toString();
+    }
+}
