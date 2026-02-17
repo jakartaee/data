9a3c6e9f0d61020f0232cce4fcd49d3c511ad47a
Nathan Rauh <nathan.rauh@us.ibm.com>
2026-02-17 10:30:49 -0600

    include BooleanLiteral

--- api/src/main/java/jakarta/data/spi/expression/literal/ComparableLiteral.java
+++ api/src/main/java/jakarta/data/spi/expression/literal/ComparableLiteral.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ * Copyright (c) 2025,2026 Contributors to the Eclipse Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -74,6 +74,7 @@ public interface ComparableLiteral<V extends Comparable<?>>
             case Short s -> (ComparableLiteral<V>) NumericLiteral.of(s);
             case BigInteger i -> (ComparableLiteral<V>) NumericLiteral.of(i);
             case BigDecimal d -> (ComparableLiteral<V>) NumericLiteral.of(d);
+            case Boolean b -> (ComparableLiteral<V>) BooleanLiteral.of(b);
             case Instant i -> (ComparableLiteral<V>) TemporalLiteral.of(i);
             case LocalDate d -> (ComparableLiteral<V>) TemporalLiteral.of(d);
             case LocalDateTime d -> (ComparableLiteral<V>) TemporalLiteral.of(d);
