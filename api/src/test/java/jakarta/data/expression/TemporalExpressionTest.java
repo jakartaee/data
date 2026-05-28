5fc3e3b9b44dc9aa68a9b1f797f0db0139189b6a
Nathan Rauh <nathan.rauh@us.ibm.com>
2026-05-28 11:04:59 -0500

    Instances of the CurrentDate temporal expression should be equal

--- api/src/test/java/jakarta/data/expression/TemporalExpressionTest.java
+++ api/src/test/java/jakarta/data/expression/TemporalExpressionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ * Copyright (c) 2025,2026 Contributors to the Eclipse Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,8 @@
  */
 package jakarta.data.expression;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import jakarta.data.constraint.Between;
 import jakarta.data.constraint.LessThan;
 import jakarta.data.mock.entity.Book;
@@ -24,6 +26,8 @@ import jakarta.data.mock.entity._Book;
 import jakarta.data.restrict.BasicRestriction;
 import jakarta.data.restrict.Restriction;
 import jakarta.data.spi.expression.function.CurrentDate;
+import jakarta.data.spi.expression.function.CurrentDateTime;
+import jakarta.data.spi.expression.function.CurrentTime;
 import jakarta.data.spi.expression.literal.TemporalLiteral;
 
 import java.time.LocalDate;
@@ -97,5 +101,23 @@ class TemporalExpressionTest {
             soft.assertThat(upperLiteral.value())
                 .isEqualTo(LocalDate.of(2025, Month.APRIL, 30));
         });
+    }
+
+    @Test
+    void testCurrentDateEqualsCurrentDate() {
+        assertEquals(CurrentDate.now(),
+                     CurrentDate.now());
+    }
+
+    @Test
+    void testCurrentDateTimeEqualsCurrentDateTime() {
+        assertEquals(CurrentDateTime.now(),
+                     CurrentDateTime.now());
+    }
+
+    @Test
+    void testCurrentTimeEqualsCurrentTime() {
+        assertEquals(CurrentTime.now(),
+                     CurrentTime.now());
     }
 }
