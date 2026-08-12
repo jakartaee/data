a6b2767af18b6906e04710c5352c2ceb7ffb515d
Otavio Santana <otaviopolianasantana@gmail.com>
2026-08-12 16:22:22 +0100

    chore: add constructors, accessors, and identity methods to MusicRecord

--- tck/src/main/java/ee/jakarta/tck/data/standalone/entity/MusicRecord.java
+++ tck/src/main/java/ee/jakarta/tck/data/standalone/entity/MusicRecord.java
@@ -17,6 +17,8 @@ package ee.jakarta.tck.data.standalone.entity;
 
 import jakarta.nosql.Id;
 
+import java.util.Objects;
+
 @jakarta.nosql.Entity
 @jakarta.persistence.Entity
 public class MusicRecord {
@@ -37,5 +39,53 @@ public class MusicRecord {
     @jakarta.nosql.Column
     private Integer releaseYear;
 
+    public MusicRecord(String catalogNumber, String title, String artist, Integer releaseYear) {
+        this.catalogNumber = catalogNumber;
+        this.title = title;
+        this.artist = artist;
+        this.releaseYear = releaseYear;
+    }
 
+    public MusicRecord() {
+    }
+
+
+    public String getCatalogNumber() {
+        return catalogNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof MusicRecord that)) {
+            return false;
+        }
+        return Objects.equals(catalogNumber, that.catalogNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(catalogNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "MusicRecord{" +
+                "catalogNumber='" + catalogNumber + '\'' +
+                ", title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                ", releaseYear=" + releaseYear +
+                '}';
+    }
 }
