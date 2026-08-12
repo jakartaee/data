ccd700f80ae69d4d00778a0bbde301c725bffd96
Otavio Santana <otaviopolianasantana@gmail.com>
2026-08-12 16:33:24 +0100

    test: add VinylRecordLifecycleObserver in standalone entity package

--- tck/src/main/java/ee/jakarta/tck/data/standalone/entity/VinylRecordLifecycleObserver.java
+++ tck/src/main/java/ee/jakarta/tck/data/standalone/entity/VinylRecordLifecycleObserver.java
@@ -15,5 +15,78 @@
  */
 package ee.jakarta.tck.data.standalone.entity;
 
-public class VinylRecordLifecycleObserver {
+import jakarta.data.event.PostDeleteEvent;
+import jakarta.data.event.PostInsertEvent;
+import jakarta.data.event.PostUpdateEvent;
+import jakarta.data.event.PostUpsertEvent;
+import jakarta.data.event.PreDeleteEvent;
+import jakarta.data.event.PreInsertEvent;
+import jakarta.data.event.PreUpdateEvent;
+import jakarta.data.event.PreUpsertEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public @ApplicationScoped
+class VinylRecordLifecycleObserver {
+
+    private final List<ObservedEvent> events = new ArrayList<>();
+
+    void onPreInsert(@Observes PreInsertEvent<MusicRecord> event) {
+        events.add(new ObservedEvent(
+                LifecycleEventType.PRE_INSERT,
+                event.entity()));
+    }
+
+    void onPostInsert(@Observes PostInsertEvent<MusicRecord> event) {
+        events.add(new ObservedEvent(
+                LifecycleEventType.POST_INSERT,
+                event.entity()));
+    }
+
+    void onPreUpdate(@Observes PreUpdateEvent<MusicRecord> event) {
+        events.add(new ObservedEvent(
+                LifecycleEventType.PRE_UPDATE,
+                event.entity()));
+    }
+
+    void onPostUpdate(@Observes PostUpdateEvent<MusicRecord> event) {
+        events.add(new ObservedEvent(
+                LifecycleEventType.POST_UPDATE,
+                event.entity()));
+    }
+
+    void onPreUpsert(@Observes PreUpsertEvent<MusicRecord> event) {
+        events.add(new ObservedEvent(
+                LifecycleEventType.PRE_UPSERT,
+                event.entity()));
+    }
+
+    void onPostUpsert(@Observes PostUpsertEvent<MusicRecord> event) {
+        events.add(new ObservedEvent(
+                LifecycleEventType.POST_UPSERT,
+                event.entity()));
+    }
+
+    void onPreDelete(@Observes PreDeleteEvent<MusicRecord> event) {
+        events.add(new ObservedEvent(
+                LifecycleEventType.PRE_DELETE,
+                event.entity()));
+    }
+
+    void onPostDelete(@Observes PostDeleteEvent<MusicRecord> event) {
+        events.add(new ObservedEvent(
+                LifecycleEventType.POST_DELETE,
+                event.entity()));
+    }
+
+    List<ObservedEvent> events() {
+        return List.copyOf(events);
+    }
+
+    void reset() {
+        events.clear();
+    }
 }
