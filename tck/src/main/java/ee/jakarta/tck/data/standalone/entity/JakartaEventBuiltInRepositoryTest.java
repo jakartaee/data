4d99a3e8ed08f51cd4dc1cd19032b5dd4a37a7c0
Otávio Santana <otaviopolianasantana@gmail.com>
2026-08-13 08:14:33 +0100

    Update tck/src/main/java/ee/jakarta/tck/data/standalone/entity/JakartaEventBuiltInRepositoryTest.java
    Co-authored-by: Nathan Rauh <nathan.rauh@us.ibm.com>

--- tck/src/main/java/ee/jakarta/tck/data/standalone/entity/JakartaEventBuiltInRepositoryTest.java
+++ tck/src/main/java/ee/jakarta/tck/data/standalone/entity/JakartaEventBuiltInRepositoryTest.java
@@ -152,8 +152,6 @@ public class JakartaEventBuiltInRepositoryTest {
             MusicRecord result = repository.save(entity);
 
             // then
-            assertThat(result).isSameAs(entity);
-
             assertThat(observer.events())
                     .containsExactly(
                             new ObservedEvent(
