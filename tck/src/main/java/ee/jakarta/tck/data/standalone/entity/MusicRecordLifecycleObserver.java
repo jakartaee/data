309daab155321b4abe251dcf5bd34364219bb988
Otávio Santana <otaviopolianasantana@gmail.com>
2026-08-13 16:00:29 +0100

    Update tck/src/main/java/ee/jakarta/tck/data/standalone/entity/MusicRecordLifecycleObserver.java
    Co-authored-by: Nathan Rauh <nathan.rauh@us.ibm.com>

--- tck/src/main/java/ee/jakarta/tck/data/standalone/entity/MusicRecordLifecycleObserver.java
+++ tck/src/main/java/ee/jakarta/tck/data/standalone/entity/MusicRecordLifecycleObserver.java
@@ -29,8 +29,8 @@ import jakarta.enterprise.event.Observes;
 import java.util.ArrayList;
 import java.util.List;
 
- @ApplicationScoped
- public class MusicRecordLifecycleObserver {
+@ApplicationScoped
+public class MusicRecordLifecycleObserver {
 
     private final List<ObservedEvent> events = new ArrayList<>();
 
