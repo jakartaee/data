843c2c4a4708745cae4e2c9e343b31302016b867
Otávio Santana <otaviopolianasantana@gmail.com>
2026-08-13 08:04:39 +0100

    Update tck/src/main/java/ee/jakarta/tck/data/standalone/entity/JakartaEventCustomRepositoryTest.java
    Co-authored-by: Nathan Rauh <nathan.rauh@us.ibm.com>

--- tck/src/main/java/ee/jakarta/tck/data/standalone/entity/JakartaEventCustomRepositoryTest.java
+++ tck/src/main/java/ee/jakarta/tck/data/standalone/entity/JakartaEventCustomRepositoryTest.java
@@ -302,7 +302,7 @@ public class JakartaEventCustomRepositoryTest {
 
             // then
             assertThat(observer.events())
-                    .containsExactly(
+                    .containsExactlyInAnyOrder(
                             event(LifecycleEventType.PRE_DELETE, first),
                             event(LifecycleEventType.PRE_DELETE, second),
                             event(LifecycleEventType.POST_DELETE, first),
