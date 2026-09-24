c59aa3ec94dfbe6679efe53bbb9cd0024d5f3c87
Nathan Rauh <nathan.rauh@us.ibm.com>
2026-09-24 09:07:37 -0500

    ensure that the entity is managed within the transaction rather than detached (3)

--- tck/src/main/java/ee/jakarta/tck/data/standalone/persistence/stateful/StatefulPersistenceEntityTests.java
+++ tck/src/main/java/ee/jakarta/tck/data/standalone/persistence/stateful/StatefulPersistenceEntityTests.java
@@ -347,6 +347,7 @@ public class StatefulPersistenceEntityTests {
 
         tran.begin();
         product = inventory.merge(product);
+        product = inventory.merge(product);
         product.setPrice(84.98);
 
         assertEquals(84.98,
